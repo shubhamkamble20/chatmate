264fa6a3fc0ff113b3f59ebe00494a49bfed88f7
shubham <shubhamkamble408@gmail.com>
2023-10-04 11:14:54 +0530

    first commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -15,7 +15,7 @@ const Welcome = () => {
   return (
     <main className="welcome">
       <h2>Welcome to Chatmate.</h2>
-      <img src="/logo512.png" alt="ReactJs logo" width={50} height={50} />
+      <img src="/logo513.png" alt="ReactJs logo" width={50} height={50} />
       <p>Sign in with Google to chat with with your fellow React Developers.</p>
       <button className="sign-in">
         <img
